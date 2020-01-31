022cb125019534e763485671a8af734b9effbb01
STZG <1600337300@qq.com>
2020-01-31 19:48:45 +0800

    init

---
+++ src/test/java/club/zstuca/platform/PlatformApplicationTests.java
@@ -0,0 +1,13 @@
+package club.zstuca.platform;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PlatformApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
